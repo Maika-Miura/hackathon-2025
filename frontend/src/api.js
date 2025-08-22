222325105107de5a07d063d5447ba5d6b37a1e77
Maika-Miura <maikam2996@gmail.com>
2025-08-22 23:30:56 +0900

    バグ修正

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 // API通信例
 export async function fetchMessage() {
-  const res = await fetch('http://localhost:5000/');
+  const res = await fetch('http://127.0.0.1:5000/');
   return res.json();
 }
